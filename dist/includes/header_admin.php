97ae38dbd2057f7e7bfc656d8c05f6c77e508ec5
michaelmcs <michaelmcs_@hotmail.com>
2023-03-07 08:56:28 -0500

    subida
    subida

---
+++ dist/includes/header_admin.php
@@ -0,0 +1,163 @@
+<header class="main-header">
+        <nav class="navbar navbar-static-top">
+          <div class="container">
+            <div class="navbar-header" style="padding-left:20px">
+			  
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                <i class="fa fa-bars"></i>
+              </button>
+            </div>
+
+            <!-- Navbar Right Menu -->
+              <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                  <!-- Tasks Menu -->
+                 <li class="">
+                    <!-- Menu Toggle Button -->
+                   <a href="home.php" class="" style="font-size:14px"><i class="glyphicon glyphicon-star-empty"></i>Horario de clase</a>
+                  </li>
+                  <!-- Tasks Menu -->
+                  <!-- Tasks Menu -->
+                 <li class="">
+                    <!-- Menu Toggle Button -->
+                   <a href="exam.php" class="" style="font-size:14px"><i class="glyphicon glyphicon-list-alt"></i> Horario del examen</a>
+                  </li>
+                  <!-- Tasks Menu -->
+				   <li class="dropdown notifications-menu">
+                    <!-- Menu toggle button -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                      <i class="glyphicon glyphicon-file"></i> Entrada
+
+                      
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <!-- Inner Menu: contains the notifications -->
+                        <ul class="menu">
+						  <li><!-- start notification -->
+                            <a href="class.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Clase
+                            </a>
+                          </li><!-- end notification -->
+						 
+                          <li><!-- start notification -->
+                            <a href="room.php">
+                              <i class="glyphicon glyphicon-scale text-green"></i> Habitación
+                            </a>
+                          </li><!-- end notification -->
+						
+						  <li><!-- start notification -->
+                            <a href="subject.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Tema
+                            </a>
+                          </li><!-- end notification -->
+						  
+						            <li><!-- start notification -->
+                            <a href="teacher.php">
+                              <i class="glyphicon glyphicon-user text-green"></i>Docente
+                            </a>
+                          </li><!-- end notification -->
+						              <li><!-- start notification -->
+                            <a href="signatories.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Firmantes
+                            </a>
+                          </li><!-- end notification -->
+
+                          <li><!-- start notification -->
+                            <a href="turno.php">
+                              <i class="glyphicon glyphicon-scale text-green"></i> turno
+                            </a>
+                          </li><!-- end notification -->
+
+                        </ul>
+                      </li>
+                     
+                    </ul>
+                  </li>
+				  <!-- Tasks Menu -->
+				   <li class="dropdown notifications-menu">
+                    <!-- Menu toggle button -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                      <i class="glyphicon glyphicon-wrench"></i> Mantenimiento
+                      
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <!-- Inner Menu: contains the notifications -->
+                        <ul class="menu">
+						  
+						  <li><!-- start notification -->
+                            <a href="department.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Departamento
+                            </a>
+                          </li><!-- end notification -->
+						  <li><!-- start notification -->
+                            <a href="designation.php">
+                              <i class="glyphicon glyphicon-cutlery text-green"></i> Designacion
+                            </a>
+                          </li><!-- end notification -->
+              <li><!-- start notification -->
+                            <a href="program.php">
+                              <i class="glyphicon glyphicon-cutlery text-green"></i> Programa
+                            </a>
+                          </li><!-- end notification -->
+						 
+						  <li><!-- start notification -->
+                            <a href="rank.php">
+                              <i class="glyphicon glyphicon-send text-green"></i> Rango
+                            </a>
+                          </li><!-- end notification -->
+                         
+						  <li><!-- start notification -->
+                            <a href="salut.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Saludo
+                            </a>
+                          </li><!-- end notification -->
+						  
+						  <li><!-- start notification -->
+                            <a href="sy.php">
+                              <i class="glyphicon glyphicon-user text-green"></i> Año escolar
+                            </a>
+                          </li><!-- end notification -->
+						  
+						  <li><!-- start notification -->
+                            <a href="time.php">
+                              <i class="glyphicon glyphicon-calendar text-green"></i> Tiempo
+                            </a>
+                          </li><!-- end notification -->
+						
+                        </ul>
+                      </li>
+                     
+                    </ul>
+                  </li>
+                  <!-- Tasks Menu -->
+					       <li class="">
+                    <!-- Menu Toggle Button -->
+                   <a href="settings.php" style="color:#fff;" class="dropdown-toggle">
+                      <i class="glyphicon glyphicon-cog text-red"></i>Configuraciones
+
+                      				
+                    </a>
+                  </li>
+                  <!-- Tasks Menu -->
+				  <li class="">
+                    <!-- Menu Toggle Button -->
+                    <a href="profile.php" class="dropdown-toggle">
+                      <i class="glyphicon glyphicon-user text-orange"></i>
+                      <?php echo $_SESSION['name'];?>
+                    </a>
+                  </li>
+                  <li class="">
+                    <!-- Menu Toggle Button -->
+                    <a href="logout.php" class="dropdown-toggle">
+                      <i class="glyphicon glyphicon-off text-red"></i> 
+                      
+                    </a>
+                  </li>
+                  
+                </ul>
+              </div><!-- /.navbar-custom-menu -->
+          </div><!-- /.container-fluid -->
+        </nav>
+      </header>
---
+++ pages/user_update.php
@@ -0,0 +1,20 @@
+ 
+<?php session_start();
+if(empty($_SESSION['id'])):
+header('Location:../index.php');
+endif;
+include('../dist/includes/dbcon.php');
+	$id = $_POST['id'];
+	$name =$_POST['name'];
+	$username =$_POST['username'];
+	$status = $_POST['status'];
+	
+	
+	mysqli_query($con,"update user set name='$name',username='$username',status='$status' where user_id='$id'")or die(mysqli_error($con));
+	
+	echo "<script type='text/javascript'>alert('Se actualizaron con éxito los detalles del usuario
+!');</script>";
+	echo "<script>document.location='user.php'</script>";  
+
+	
+?>
